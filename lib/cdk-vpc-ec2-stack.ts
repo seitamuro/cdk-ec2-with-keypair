553427bc8d1b0218582f4e29676fd1faa4565c42
seitamuro <seitamuro0210@gmail.com>
2024-04-18 23:45:02 +0900

    add keypair configuration

--- lib/cdk-vpc-ec2-stack.ts
+++ lib/cdk-vpc-ec2-stack.ts
@@ -15,14 +15,36 @@ export class CdkVpcEc2Stack extends cdk.Stack {
     });
 
     const vpc = new ec2.Vpc(this, "SampleVpc", {
+      maxAzs: 1,
+      natGateways: 0,
       ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
       vpcName: "cdk-sample-vpc",
+      subnetConfiguration: [
+        {
+          name: "PublicSubnet",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
+    });
+
+    const cfnKeyPair = new ec2.CfnKeyPair(this, "SampleKeyPair", {
+      keyName: "test-key-pair",
+    });
+    cfnKeyPair.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
+
+    new cdk.CfnOutput(this, "GetSSHKeyCommand", {
+      value: `aws ssm get-parameter --name /ec2/keypair/${cfnKeyPair.getAtt(
+        "KeyPairId"
+      )} --region ${
+        this.region
+      } --with-decryption --query Parameter.Value --output text`,
     });
 
     const securityGroup = new ec2.SecurityGroup(this, "SampleSecurityGroup", {
       vpc: vpc,
       securityGroupName: "cdk-vpc-ec2-security-group",
     });
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22));
 
     const instanceRole = new iam.Role(this, "SampleRole", {
       assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com"),
@@ -47,8 +69,11 @@ export class CdkVpcEc2Stack extends cdk.Stack {
         ),
         machineImage: ec2.MachineImage.latestAmazonLinux2(),
         securityGroup: securityGroup,
-        role: instanceRole,
-        instanceName: name,
+        keyPair: ec2.KeyPair.fromKeyPairName(
+          this,
+          `EC2${name}KeyPair`,
+          "test-key-pair"
+        ),
       });
     };
 
@@ -57,10 +82,13 @@ export class CdkVpcEc2Stack extends cdk.Stack {
       "cdk-vpc-ec2-instance1",
       vpc.selectSubnets({ subnetType: ec2.SubnetType.PUBLIC })
     );
-    const instance2 = createInstance(
+    new cdk.CfnOutput(this, "Instance1PublicIp", {
+      value: instance1.instancePublicIp,
+    });
+    /*const instance2 = createInstance(
       "SampleInstance2",
       "cdk-vpc-ec2-instance2",
       vpc.selectSubnets({ subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS })
-    );
+    );*/
   }
 }
